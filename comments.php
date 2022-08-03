77d1a2556fe3feee071cbf20abfda7a58168883a
Zeljko Klasnja <zeljko.klasnja@gmail.com>
2022-08-03 21:23:55 +0200

    Zadatak 5

--- comments.php
+++ comments.php
@@ -1,16 +1,25 @@
+<?php
+include_once('db-connect.php');
+
+$sql = "SELECT author, created_at, text, post_id FROM comments WHERE post_id = {$_GET['post_id']}";
+$statement = $connection->prepare($sql);
+$statement->execute();
+$comments = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <div class="comments">
-
-    <!-- zameniti testne komentare sa pravim komentarima koji pripadaju blog post-u iz baze -->
-
     <div class="single-comment">
         <ul>
+            <?php foreach ($comments as $comment) { ?>
             <li>
-                <div>posted by: <strong>Comment Author</strong> on 2022-08-03</div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                    <div class="single-comment">
+                        <div>posted by: <strong><?php echo $comment['author']; ?></strong> on <?php echo $comment['created_at']; ?></div>
+                        <div><?php echo $comment['text']; ?></div>
+                    </div>
             </li>
+            <hr>
+            <?php } ?>
         </ul>
     </div>
 </div>
--- posts.php
+++ posts.php
@@ -42,18 +42,18 @@
             <div class="col-sm-8 blog-main">
 
                 <div class="blog-post">
-                <?php
-    foreach ($posts as $post){?>
-                    <h2 class="blog-post-title"><a href="single-post.php?post_id=<?php echo ($post['id']) ?>"><?php echo ($post['title']) ?></a></h2>
-                    <p class="blog-post-meta"><?php echo ($post['created_at']) ?> by <a href="#"><?php echo ($post['author']) ?></a></p>
-                    <p><?php echo ($post['body']);?></p>
-                    <?php } ?>
-                
-                <nav class="blog-pagination">
-                    <a class="btn btn-outline-primary" href="#">Older</a>
-                    <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-                </nav>
-
+                    <?php
+                    foreach ($posts as $post){?>
+                        <h2 class="blog-post-title"><a href="single-post.php?post_id=<?php echo ($post['id']) ?>"><?php echo ($post['title']) ?></a></h2>
+                        <p class="blog-post-meta"><?php echo ($post['created_at']) ?> by <a href="#"><?php echo ($post['author']) ?></a></p>
+                        <p><?php echo ($post['body']);?></p>
+                        <?php } ?>
+                    
+                    <nav class="blog-pagination">
+                        <a class="btn btn-outline-primary" href="#">Older</a>
+                        <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+                    </nav>
+                </div>
             </div><!-- /.blog-main -->
 
             <?php include('sidebar.php') ?>
